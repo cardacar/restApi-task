53689b1cf68f5697b5b345d958427a41012ecdaf
Carlos Quiros <carlosquiros12@gmail.com>
2021-01-07 16:47:03 -0500

    Metodo POST creado

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ import taskRoutes from "./routes/taskRoute";
 
 const app = express();
 
+//Settings
 app.set('port', process.env.PORT ||3000);
 
+app.use(express.json())
+
+//routes
 app.get('/', (req,res) =>{
     res.json({message: "Welcome to my application"});
 });
--- src/routes/taskRoute.js
+++ src/routes/taskRoute.js
@@ -1,8 +1,16 @@
 import {Router} from 'express'
+import Task from '../models/Task'
 const router = Router()
 
 router.get('/', (req,res)=>{
     res.send('task')
 });
+
+router.post('/', async (req,res)=>{
+    const newTask = new Task({title: req.body.title, description: req.body.description})
+    await newTask.save();
+    console.log(newTask)
+    res.json('New task create')
+})
 
 export default router;
